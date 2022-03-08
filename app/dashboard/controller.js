57911b0c8cd6d50f07e0c210745480af432a9a2e
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2022-03-08 10:39:47 +0800

    mengelola halaman dashboard dan menambahkan model player

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,9 +1,25 @@
+const Transaction = require('../transaction/model');
+const Voucher = require('../voucher/model');
+const Player = require('../player/model');
+const Category = require('../category/model');
+
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render('index', {
+
+      let transaction = await Transaction.countDocuments();
+      let voucher = await Voucher.countDocuments();
+      let player = await Player.countDocuments();
+      let category = await Category.countDocuments();
+      res.render('admin/dashboard/view_dashboard', {
         name: req.session.user.name,
-        title: 'Halaman Dashboard'
+        title: 'Halaman Dashboard',
+        count: {
+          transaction,
+          voucher,
+          player,
+          category
+        }
       })
     } catch (err) {
       console.log(err);
