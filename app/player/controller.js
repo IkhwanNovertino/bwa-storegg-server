871d25127a39eca25f43c8df6d8de7016137c8f1
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2022-04-11 10:42:48 +0800

    create api historyTransaction, detailHistoryTransaction, dashboard, profile, and updateProfile

--- app/player/controller.js
+++ app/player/controller.js
@@ -5,6 +5,9 @@ const Nominal = require('../nominal/model');
 const Payment = require('../payment/model');
 const Bank = require('../bank/model');
 const Transaction = require('../transaction/model');
+const path = require('path');
+const fs = require('fs');
+const config = require('../../config');
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -102,6 +105,184 @@ module.exports = {
 
     } catch (err) {
       res.status(500).json({ message: err.message || 'Terjadi kesalahan pada server' });
+    }
+  },
+  history: async (req, res) => {
+    try {
+      const { status = '' } = req.query;
+
+      let criteria = {}
+
+      if (status.length) {
+        criteria = {
+          ...criteria,
+          status: { $regex: `${status}`, $options: 'i' }
+        }
+      }
+
+      if (req.player._id) {
+        criteria = {
+          ...criteria,
+          player: req.player._id
+        }
+      }
+
+      const history = await Transaction.find(criteria)
+
+      let total = await Transaction.aggregate([
+        { $match: criteria },
+        {
+          $group: {
+            _id: null,
+            value: { $sum: "$value" }
+          }
+        }
+      ])
+
+      res.status(200).json({
+        data: history,
+        total: total.length ? total[0].value : 0
+      })
+
+    } catch (err) {
+      res.status(500).json({ message: err.message || 'Terjadi kesalahan pada server' });
+    }
+  },
+
+  historyDetail: async (req, res) => {
+    try {
+      const { id } = req.params
+
+      const history = await Transaction.findOne({ _id: id })
+
+      if (!history) return res.status(404).json({ message: 'History transaksi tidak ditemukan' })
+
+      res.status(200).json({ data: history })
+
+    } catch (err) {
+      res.status(500).json({ message: err.message || 'Terjadi kesalahan pada server' });
+    }
+  },
+
+  dashboard: async (req, res) => {
+    try {
+      const count = await Transaction.aggregate([
+        { $match: { player: req.player._id } },
+        {
+          $group: {
+            _id: '$category',
+            value: { $sum: '$value' }
+          }
+        }
+      ])
+
+      const category = await Category.find({});
+
+      category.forEach(element => {
+        count.forEach(data => {
+          if (data._id.toString() === element._id.toString) {
+            data.name = element.name;
+          }
+        })
+      })
+
+      const history = await Transaction.find({ player: req.player._id })
+        .populate('category')
+        .sort({ 'updateAt': -1 })
+
+      res.status(200).json({ data: history, count: count });
+    } catch (err) {
+      res.status(500).json({ message: err.message || 'Terjadi kesalahan pada server' });
+    }
+  },
+
+  profile: async (req, res) => {
+    try {
+
+      const player = {
+        id: req.player._id,
+        username: req.player.username,
+        name: req.player.name,
+        email: req.player.email,
+        phone_number: req.player.phoneNumber,
+        avatar: req.player.avatar
+      }
+
+      res.status(200).json({ data: player });
+    } catch (err) {
+      res.status(500).json({ message: err.message || 'Terjadi kesalahan pada server' });
+    }
+  },
+  editProfile: async (req, res, next) => {
+    try {
+      const { name = "", phoneNumber = "" } = req.body
+
+      const payload = {}
+
+      if (name.length) payload.name = name
+      if (phoneNumber.length) payload.phoneNumber = phoneNumber
+
+      if (req.file) {
+        let tmp_path = req.file.path;
+        let originalExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
+        let filename = req.file.filename + '.' + originalExt;
+        let target_path = path.resolve(config.rootPath, `public/uploads/${filename}`);
+
+        const src = fs.createReadStream(tmp_path);
+        const dest = fs.createWriteStream(target_path);
+
+        src.pipe(dest)
+        src.on('end', async () => {
+          let player = await Player.findOne({ _id: req.player._id });
+
+          let currentImage = `${config.rootPath}/public/uploads/${player.avatar}`;
+          if (fs.existsSync(currentImage)) {
+            fs.unlinkSync(currentImage);
+          }
+
+          player = await Player.findOneAndUpdate(
+            { _id: req.player._id },
+            {
+              ...payload,
+              avatar: filename
+            }, { new: true, runValidators: true }
+          )
+          res.status(201).json({
+            data: {
+              id: player.id,
+              name: player.name,
+              phoneNumber: player.phoneNumber,
+              avatar: player.avatar,
+            }
+          })
+        })
+
+        src.on('err', async () => {
+          next(err);
+        })
+      } else {
+        const player = await Player.findOneAndUpdate(
+          { _id: req.player._id },
+          payload, { new: true, runValidators: true }
+        )
+
+        res.status(201).json({
+          data: {
+            id: player.id,
+            name: player.name,
+            phoneNumber: player.phoneNumber,
+            avatar: player.avatar,
+          }
+        })
+      }
+    } catch (err) {
+      if (err & err.name === "ValidationError") {
+        res.status(422).json({
+          error: 1,
+          message: err.message,
+          fields: err.errors
+        })
+      }
     }
   }
 }
